f4f39fea11a0787c4c9e7e38881d5123ef880996
Yann VERY <yann.very@gmail.com>
2016-10-12 20:34:25 +0200

    Passed test verse 1

--- beer_song.rb
+++ beer_song.rb
@@ -1,8 +1,18 @@
 class BeerSong
-  def verse(number)
+  def verse(verse_number)
     <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+#{verse_number} #{ verse_number == 1 ? 'bottle' : 'bottles'} of beer on the wall, #{verse_number} #{ verse_number == 1 ? 'bottle' : 'bottles'} of beer.
+#{how_many_on_the_wall(verse_number)}
 VERSE
+  end
+
+  def how_many_on_the_wall(verse_number)
+    if verse_number > 2
+      "Take one down and pass it around, #{verse_number - 1} bottles of beer on the wall."
+    elsif verse_number == 1
+      "Take it down and pass it around, no more bottles of beer on the wall."
+    else
+      "Take one down and pass it around, 1 bottle of beer on the wall."
+    end
   end
 end
