ff7bf26c18070f79a1dda18f180376383a79fa14
AxelRD <axel.diore@gmail.com>
2016-10-15 18:59:40 +0200

    test complet

--- beer_song.rb
+++ beer_song.rb
@@ -1,53 +1,35 @@
 class BeerSong
+  def lyrics
+    verses(99,0)
+  end
 
   def verses(first,last)
     song = []
-    (last..first).to_a.reverse.each do |verse_num|
-      song << verse(verse_num)
+    (last..first).to_a.reverse.each do |verse_number|
+      song << verse(verse_number)
     end
     song.join("\n")
   end
 
   def verse(verse_number)
-    <<-VERSE
-#{nombre_de_conteneurs(verse_number).capitalize} #{conteneur(verse_number)} of beer on the wall, #{nombre_de_conteneurs(verse_number)} #{conteneur(verse_number)} of beer.
-#{how_many_on_the_wall(verse_number)}
-VERSE
-  end
-
-  def how_many_on_the_wall(verse_number)
-    "#{take_it_or_go_to_the_store(verse_number)} #{how_many_left(verse_number)} #{conteneur(how_many_left(verse_number))} of beer on the wall."
-  end
-
-  def take_it_or_go_to_the_store(verse_number)
-    return 'Go to the store and buy some more,' if verse_number == 0
-    return 'Take it down and pass it around,' if verse_number == 1
-    'Take one down and pass it around,'
-  end
-
-  def conteneur(number)
-    case number
-    when 1
-      'bottle'
+    case
+    when verse_number == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when verse_number == 1
+      "#{verse_number} #{container(verse_number)} of beer on the wall, #{verse_number} #{container(verse_number)} of beer.\n"\
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
     else
-      'bottles'
+      "#{verse_number} #{container(verse_number)} of beer on the wall, #{verse_number} #{container(verse_number)} of beer.\n"\
+      "Take one down and pass it around, #{verse_number - 1} #{container(verse_number - 1)} of beer on the wall.\n"
     end
   end
 
-  def nombre_de_conteneurs(number)
-    return 'no more' if number == 0
-    number.to_s
+  def container(number)
+    return "bottles" if number > 1
+    "bottle"
   end
-
-  def supprime_conteneur(verse_number)
-    return 'one' if verse_number > 2
-    'it'
-  end
-
-  def how_many_left(verse_number)
-    return 99 if verse_number == 0
-    return 'no more' if verse_number == 1
-    (verse_number - 1)
-  end
-
+end
+class BookKeeping
+  VERSION = 2
 end
